69220a7382b6c2e492cb094d154d8573d7ae8529
Allan D <afdeltour@gmail.com>
2023-05-05 23:50:50 +0200

    ADD Dynamic URL MOD Error Deployement

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,10 @@
-import { Buttons } from '../components/Buttons/Buttons'
+// export const Home = () => {
+//     return (
+//         <>
+//             <h1 className="text-4xl font-bold m-4"> What Pokemon are you looking for ? </h1>
 
-export const Home = () => {
-    return (
-        <>
-            <h1 className="text-4xl font-bold m-4"> What Pokemon are you looking for ? </h1>
+//             <input type="text" placeholder="Search Pokemon, move, ability, etc." className="input input-bordered w-full max-w-xs mx-6 mb-10" />
 
-            <input type="text" placeholder="Search Pokemon, move, ability, etc." className="input input-bordered w-full max-w-xs mx-6 mb-10" />
-
-            <Buttons/>
-        </>
-    )
-}
+//         </>
+//     )
+// }
